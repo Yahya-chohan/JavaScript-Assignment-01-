ebacbd5a67b1e9e7ea9295abd2052e9dfd2bb210
Yahya-Chohan <yahyachohan123@gmail.com>
2025-01-21 14:10:07 +0500

    add css

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let prise = 500
+
+let show = document.querySelector(".result");
+
 function prisecalculater() {
-    let weight = prompt(("how much chicken do you need?"));
+    let weight = prompt("how much chicken do you need?");
     let weigthinkg = weight /1000 ;
     let priseinkg = weigthinkg *prise ;
-alert(`You will have to pay ${priseinkg} Rs for ${weight}grams of chicken `)
+    show.innerHTML=`You will have to pay ${priseinkg} Rs for ${weight}grams of chicken `;
+    // document.getElementsByClass("result") .inn (`You will have to pay ${priseinkg} Rs for ${weight}grams of chicken `)
+    
 }
-
-
+console.log (typeof show)
